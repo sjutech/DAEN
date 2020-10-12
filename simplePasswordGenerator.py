fe382ee97c7acb00b270455fdb11cb7640213e0a
sjutech <70280072+sjutech@users.noreply.github.com>
2020-10-12 11:28:03 -0400

    Add files via upload

---
+++ simplePasswordGenerator.py
@@ -0,0 +1,6 @@
+#Simple Password Generator
+place  = input("Enter a place:")
+special = input("Enter special characters:")
+number = input("Enter a number:")
+
+print(place + special + "{}".format(number))
